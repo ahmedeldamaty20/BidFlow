d9b6a032ad5ff25397e3dcc9a8ced3d4f3f5939f
ahmedeldamaty20 <ahmedeldamaty20@gmail.com>
2026-09-10 13:56:41 +0300

    Add Auction and Bid entity classes with basic properties

---
+++ src/BidFlow.Domain/Entities/Bid.cs
@@ -0,0 +1,14 @@
+﻿namespace BidFlow.Domain.Entities;
+
+public class Bid
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid AuctionId { get; set; }
+    public Auction? Auction { get; set; }
+
+    // Temporary: Bidder name as free text. Will be changed to BidderId linked with JWT in Flow 5
+    public string BidderName { get; set; } = string.Empty;
+
+    public decimal Amount { get; set; }
+    public DateTime PlacedAt { get; set; } = DateTime.UtcNow;
+}
